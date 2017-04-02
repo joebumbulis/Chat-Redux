3fed353bff68a8bb90a8a18f8ec856f0a17f0f57
Joe Bumbulis <joebumbulis@gmail.com>
2017-04-02 09:40:11 -0500

    added more js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,11 @@ import {
     createStore
 } from 'redux';
 import loginView from './login_view.js'
+import appView from './app_view.js'
 
 export default function app() {
+  const url = 'http://tiny-za-server.herokuapp.com/collections/joebum_chat/'
+
     const initialState = {
         user: "Guest User",
         messages: [],
@@ -23,17 +26,59 @@ export default function app() {
                 return state
 
             case "LOG_IN":
+                console.log("I did log in")
                 var newUser = action.user;
-                return Object.assign({}, state, {
-                    user: newUser,
-                    view: appView
-                }); //makes a copy of the state before returning it
+                if (newUser === '') {
+                    alert('Please choose a username')
+                } else {
+                    return Object.assign({}, state, {
+                        user: newUser,
+                        view: appView
+                    }); //makes a copy of the state before returning it
+                }
 
-                // case "REMOVE_ITEM":
-                //     newItems.splice(action.index, 1);
-                //     return Object.assign({}, state, {
-                //         items: newItems
-                //     });
+                //get json request here to populate log in page. with time out.
+                $.getJSON(url).then((data, status, xhr) =>{
+                  setTimeout( () => {
+                    store.dispatch({ type: "MESSAGES_LOADED", msgs: data })
+                  }, 2000);
+
+                  //this timeout is for loading animation???
+                })
+                return Object.assign({}, state,  newState);
+                console.log(Newstate);
+
+            // case "LOAD_MESSAGES":
+            //     $.getJSON(url).then((date) =>{
+            //       store.dispatch({ type: "MESSAGES_LOADED", msgs: data})
+            //     });
+            //     return state;
+
+            case "MESSAGES_LOADED":
+                var newState = {
+                  msgs: action.msgs,
+                  loadingMsgs: false
+                }
+                return Object.assign({}, state, newState)
+
+                //
+                //       case "TODOS_LOADED":
+                //         var newState = {
+                //            todos: action.todos,
+                //            loadingTodos: false
+                //         };
+                //         return Object.assign({}, currentState, newState);
+
+            // case "SEND_MESSAGE":
+            //     console.log("I did case send message")
+            //     var newMsg = action.msg;
+            //     return Object.assign({}, state, {
+            //         items: newItems
+            //     });
+            //     //
+            //     case "GET_CHATS":
+            //     console.log("I did get chats")
+
 
             default:
                 return state;
@@ -44,15 +89,21 @@ export default function app() {
 
     const render = function() {
         let state = store.getState();
+        // console.log(state);
         $('#app').html(state.view(store))
         console.log(state)
     }
 
-    store.subscribe(render)
+
+
+    store.subscribe(render);
+
+
 
     store.dispatch({
-        type: "TESTING"
+        type: "TESTING",
     });
+
     // store.dispatch({
     //     type: "LOG_IN",
     //     item: 'apples'
--- app/scripts/app_view.js
+++ app/scripts/app_view.js
@@ -1,1 +1,30 @@
 export default function (store){
+
+  let $html = $(`
+  <header>
+    <h1>Joe's Chat Redux</h1>
+  </header>
+  <div class="message-page page">
+      <div class="messages-container">
+          <ul id="messages-container">
+          </ul>
+
+      </div>
+      <div class="input-container">
+          <input class="message-input" type="text" value="">
+          <button class="send-btn">send</button>
+      </div>
+  </div>
+  <footer>
+    <h5>produced by Joe Bumbulis</h5>
+  </footer>
+  `);
+
+  $($html).find(".send-btn").on('click', (e) => {
+      store.dispatch({
+          type: "SEND_MESSAGE",
+          msg: $(html).find('.message-input').val()
+      });
+  });
+  return $html;
+  }
